4aedd480cc0a6c50cbb6d914cd220313887e07ea
rajumb0232 <rajugowda0232@gmail.com>
2023-05-17 15:57:33 +0530

    project cerated

---
+++ src/main/java/edu/goshop_ecoomerce/GoShopECommerceApiApplication.java
@@ -0,0 +1,13 @@
+package edu.goshop_ecoomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoShopECommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoShopECommerceApiApplication.class, args);
+	}
+
+}
