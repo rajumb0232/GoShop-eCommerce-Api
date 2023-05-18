5d15a30b63b84cbe682da313c723de6c5dc3cf2e
rajumb0232 <rajugowda0232@gmail.com>
2023-05-18 13:03:28 +0530

    created required entities and enums

---
+++ src/main/java/edu/goshop_ecoomerce/entity/User.java
@@ -0,0 +1,42 @@
+package edu.goshop_ecoomerce.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import edu.goshop_ecoomerce.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userId;
+	private String userFirstName;
+	private String userSecondName;
+	private long userPhoneNumber;
+	private String UserEmail;
+	private String userPassword;
+	private LocalDateTime userCreatedDateTime;
+	private UserRole userRole;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Address> addresses;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Product> products;
+	
+	@OneToMany(mappedBy = "user")
+	private List<CustomerProduct> customerProducts;
+	
+	@OneToMany(mappedBy = "user")
+	private List<CustomerOrder> customerOrders;
+}
