4b27b9254a95f94789a32746f1ebccc4d2ddf3b7
akashgudase <gudase.akash@gmail.com>
2023-05-18 13:48:43 +0530

    added swagger

---
+++ src/main/java/edu/goshop_ecoomerce/util/ApplicationConfiguration.java
@@ -0,0 +1,42 @@
+package edu.goshop_ecoomerce.util;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class ApplicationConfiguration {
+
+	@Bean
+	public OpenAPI getusersMicroserviceOpenAPI() {
+
+		Server devServer = new Server();
+		devServer.setUrl("http://localhost:8080");
+		devServer.setDescription("Server URL in Development environment");
+
+		Server prodServer = new Server();
+		prodServer.setUrl("http://localhost:8080/swagger-ui/index.html");
+		prodServer.setDescription("Swagger URL in Production environment");
+
+		Contact contact = new Contact();
+		contact.setEmail("info@AppDomainName.in");
+		contact.setName("GoShop-eCommerce-Api");
+		contact.setUrl("https://www.AppDomainName.in");
+
+		License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+		Info info = new Info().title("GoShop-eCommerce-Api RESTful web service documentation").version("1.0")
+				.contact(contact).description("This API exposes endpoints to manage GoShop-eCommerce-Api.")
+				.termsOfService("https://www.AppDomainName.com/terms").license(mitLicense);
+
+		return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+	}
+
+}
