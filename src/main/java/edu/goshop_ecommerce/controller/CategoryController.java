a5e8bd1742917382d74754a76c520bfb5c561c08
akashgudase <gudase.akash@gmail.com>
2023-05-20 09:58:38 +0530

    Brand and Category controllers added

--- src/main/java/edu/goshop_ecommerce/controller/CategoryController.java
+++ src/main/java/edu/goshop_ecommerce/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,6 @@ import edu.goshop_ecommerce.dto.CategoryRequest;
 import edu.goshop_ecommerce.dto.CategoryResponse;
 import edu.goshop_ecommerce.service.CategoryService;
 import edu.goshop_ecommerce.util.ResponseStructure;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/categories")
@@ -22,8 +22,9 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 
-	@PostMapping()
-	public ResponseEntity<ResponseStructure<CategoryResponse>> addCategory(CategoryRequest categoryRequest) {
+	@PostMapping
+	public ResponseEntity<ResponseStructure<CategoryResponse>> addCategory(
+			@RequestBody CategoryRequest categoryRequest) {
 		return categoryService.addCategory(categoryRequest);
 	}
 
