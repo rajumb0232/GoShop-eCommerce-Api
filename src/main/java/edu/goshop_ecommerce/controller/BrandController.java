ae5f7338277fbf4fe7e1c87de99042a3692e2b58
rajumb0232 <rajugowda0232@gmail.com>
2023-10-04 16:35:03 +0530

    Code optimized in controller and Service layers of Brand and Address

--- src/main/java/edu/goshop_ecommerce/controller/BrandController.java
+++ src/main/java/edu/goshop_ecommerce/controller/BrandController.java
@@ -2,61 +2,63 @@ package edu.goshop_ecommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.goshop_ecommerce.request_dto.BrandRequest;
 import edu.goshop_ecommerce.request_dto.BrandResponse;
 import edu.goshop_ecommerce.service.BrandService;
+import edu.goshop_ecommerce.util.ErrorStructure;
 import edu.goshop_ecommerce.util.ResponseStructure;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/brands")
 @Tag(name = "Brand", description = "Brand REST API's")
 public class BrandController {
 
 	@Autowired
 	private BrandService brandService;
 
-	@ApiResponses({
+	@Operation(description = "**Create Brand -** the API endpoint is used to create a Brand", responses = {
 			@ApiResponse(responseCode = "201", description = "brand added", content = {
 					@Content(schema = @Schema(implementation = BrandResponse.class)) }),
 			@ApiResponse(responseCode = "400", description = "failed to add brand", content = {
-					@Content(schema = @Schema) }) })
+					@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('MERCHANT') OR hasAuthority('ADMINISTRATOR')")
 	@PostMapping("/{merchantId}")
-	public ResponseEntity<ResponseStructure<BrandResponse>> addBrand(@PathVariable long merchantId,
-			@RequestBody BrandRequest brandRequest) {
-		return brandService.addBrand(merchantId, brandRequest);
+	public ResponseEntity<ResponseStructure<BrandResponse>> addBrand(@RequestBody BrandRequest brandRequest) {
+		return brandService.addBrand(brandRequest);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "brand updated", content = {
-					@Content(schema = @Schema(implementation = BrandResponse.class)) }),
-			@ApiResponse(responseCode = "400", description = "failed to update brand", content = {
-					@Content(schema = @Schema) }) })
+	@Operation(description = "**Update Brand By Id -**"
+			+ " the API endpoint is used to update the Brand details based on the Id", responses = {
+					@ApiResponse(responseCode = "200", description = "brand updated", content = {
+							@Content(schema = @Schema(implementation = BrandResponse.class)) }),
+					@ApiResponse(responseCode = "400", description = "failed to update brand", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('MERCHANT') OR hasAuthority('ADMINISTRATOR')")
 	@PutMapping("/{brandId}")
 	public ResponseEntity<ResponseStructure<BrandResponse>> updateBrand(@PathVariable long brandId,
 			@RequestBody BrandRequest brandRequest) {
 		return brandService.updateBrand(brandId, brandRequest);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "brand deleted", content = {
-					@Content(schema = @Schema(implementation = BrandResponse.class)) }),
-			@ApiResponse(responseCode = "400", description = "failed to delete brand", content = {
-					@Content(schema = @Schema) }),
-			@ApiResponse(responseCode = "404", description = "brand not found", content = {
-					@Content(schema = @Schema) }) })
+	@Operation(description = "**Delete Brand By Id -**"
+			+ " the API endpoint is used to delete brand data based on the Id.", responses = {
+					@ApiResponse(responseCode = "200", description = "brand updated", content = {
+							@Content(schema = @Schema(implementation = BrandResponse.class)) }),
+					@ApiResponse(responseCode = "400", description = "failed to update brand", content = {
+							@Content(schema = @Schema) }) })
+	@PreAuthorize("hasAuthority('MERCHANT') OR hasAuthority('ADMINISTRATOR')")
 	@DeleteMapping("/{brandId}")
 	public ResponseEntity<ResponseStructure<BrandResponse>> deleteBrand(@PathVariable long brandId) {
 		return brandService.deleteBrandById(brandId);
--- src/main/java/edu/goshop_ecommerce/service/AddressService.java
+++ src/main/java/edu/goshop_ecommerce/service/AddressService.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import edu.goshop_ecommerce.dao.AddressDao;
@@ -14,7 +16,7 @@ import edu.goshop_ecommerce.entity.Address;
 import edu.goshop_ecommerce.entity.User;
 import edu.goshop_ecommerce.enums.UserRole;
 import edu.goshop_ecommerce.exception.AddressNotFoundById;
-import edu.goshop_ecommerce.exception.UserNotFoundByIdException;
+import edu.goshop_ecommerce.repo.UserRepo;
 import edu.goshop_ecommerce.request_dto.AddressRequest;
 import edu.goshop_ecommerce.response_dto.AddressResponse;
 import edu.goshop_ecommerce.util.ResponseStructure;
@@ -27,58 +29,47 @@ public class AddressService {
 	private ModelMapper modelMapper;
 	@Autowired
 	private UserDao userDao;
-	
-	public ResponseEntity<ResponseStructure<AddressResponse>> addAddress(AddressRequest addressRequest){
+	@Autowired
+	private UserRepo userRepo;
+
+	public ResponseEntity<ResponseStructure<AddressResponse>> addAddress(AddressRequest addressRequest) {
 		Address address = this.modelMapper.map(addressRequest, Address.class);
-		User user = userDao.findUserById(userId);
-		if(user !=null) {
-			if(user.getUserRole().equals(UserRole.CUSTOMER)) {
-				user.getAddresses().add(address);
-				address = addressDao.saveAddress(address);
-				user = userDao.addUser(user);
-				
-				AddressResponse addressResponse=  this.modelMapper.map(address,AddressResponse.class);
-				ResponseStructure<AddressResponse> structure = new ResponseStructure<>();
-				structure.setData(addressResponse);
-				structure.setMessage("address saved success to a user");
-				structure.setStatus(HttpStatus.OK.value());
-				return new ResponseEntity<ResponseStructure<AddressResponse>>(structure,HttpStatus.OK);
-			}else {
-				
-				List<Address> addresses = user.getAddresses();
-				for(Address exAddress : addresses) {
-					addresses.remove(exAddress);
-					address.setAddressId(exAddress.getAddressId());
-					addresses.add(address);
-				}
-				user.setAddresses(addresses);
-				address = addressDao.saveAddress(address);
-				user = userDao.addUser(user);
-				AddressResponse addressResponse=  this.modelMapper.map(address,AddressResponse.class);
-				ResponseStructure<AddressResponse> structure = new ResponseStructure<>();
-				structure.setData(addressResponse);
-				structure.setMessage("address saved success to a user");
-				structure.setStatus(HttpStatus.OK.value());
-				return new ResponseEntity<ResponseStructure<AddressResponse>>(structure,HttpStatus.OK);
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String userEmail = authentication.getName();
+		User user = userRepo.findByUserEmail(userEmail);
+		if (user.getUserRole().equals(UserRole.CUSTOMER)) {
+			user.getAddresses().add(address);
+		} else {
+			List<Address> addresses = user.getAddresses();
+			for (Address exAddress : addresses) {
+				addresses.remove(exAddress);
+				address.setAddressId(exAddress.getAddressId());
+				addresses.add(address);
 			}
-			
+			user.setAddresses(addresses);
 		}
-		throw new UserNotFoundByIdException("user not found by given user id");
+		address = addressDao.saveAddress(address);
+		user = userDao.addUser(user);
+		AddressResponse addressResponse = this.modelMapper.map(address, AddressResponse.class);
+		ResponseStructure<AddressResponse> structure = new ResponseStructure<>();
+		structure.setData(addressResponse);
+		structure.setMessage("Added address successfully to the user");
+		structure.setStatus(HttpStatus.OK.value());
+		return new ResponseEntity<ResponseStructure<AddressResponse>>(structure, HttpStatus.OK);
 	}
-	
-	public ResponseEntity<ResponseStructure<AddressResponse>> getAddress(long addressId){
+
+	public ResponseEntity<ResponseStructure<AddressResponse>> getAddress(long addressId) {
 		Address address = addressDao.findAddress(addressId);
-		if(address!=null) {
-			AddressResponse addressResponse=  this.modelMapper.map(address,AddressResponse.class);
+		if (address != null) {
+			AddressResponse addressResponse = this.modelMapper.map(address, AddressResponse.class);
 			ResponseStructure<AddressResponse> structure = new ResponseStructure<>();
 			structure.setData(addressResponse);
 			structure.setMessage("address found successfully");
 			structure.setStatus(HttpStatus.FOUND.value());
-			return new ResponseEntity<ResponseStructure<AddressResponse>>(structure,HttpStatus.FOUND);
+			return new ResponseEntity<ResponseStructure<AddressResponse>>(structure, HttpStatus.FOUND);
 		}
-		 throw new AddressNotFoundById("address not found for given id");
+		throw new AddressNotFoundById("address not found for given id");
 	}
-	
-	
 
 }
