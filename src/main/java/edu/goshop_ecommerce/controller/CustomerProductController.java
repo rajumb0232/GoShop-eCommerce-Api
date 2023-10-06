a374f4d94d905778cb95df9bac5cd09670c4e3cd
rajumb0232 <rajugowda0232@gmail.com>
2023-10-06 13:17:33 +0530

    updated CustomerProduct controller and service layer

--- src/main/java/edu/goshop_ecommerce/controller/CustomerProductController.java
+++ src/main/java/edu/goshop_ecommerce/controller/CustomerProductController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,59 +17,69 @@ import edu.goshop_ecommerce.enums.BuyStatus;
 import edu.goshop_ecommerce.enums.Priority;
 import edu.goshop_ecommerce.response_dto.CustomerProductResponse;
 import edu.goshop_ecommerce.service.CustomerProductService;
+import edu.goshop_ecommerce.util.ErrorStructure;
 import edu.goshop_ecommerce.util.ResponseStructure;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/customerProduct")
-@Tag(name = "CustomerProduct", description = "CustomerProduct REST API's")
+@Tag(name = "CustomerProduct", description = "REST endpoints")
 public class CustomerProductController {
 
 	@Autowired
 	private CustomerProductService customerproductService;
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "201", description = "customerProduct added", content = {
-					@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
-			@ApiResponse(responseCode = "400", description = "failed to add customerProduct", content = {
-					@Content(schema = @Schema) }) })
-	@PostMapping
+	@Operation(description = "**Create CustomerProduct -**"
+			+ " the API endpoint is used to create a customer product with preferred Priority place (WISHLIST or CART),"
+			+ " (authority - CUSTOMER)", responses = {
+					@ApiResponse(responseCode = "201", description = "successfully created customerProduct", content = {
+							@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
+					@ApiResponse(responseCode = "400", description = "failed to create customerProduct", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('CUSTOMER')")
+	@PostMapping("/priority/{priority}/customer-products/products/{productId}")
 	public ResponseEntity<ResponseStructure<CustomerProductResponse>> addCustomerProduct(
-			@RequestParam Priority priority, @RequestParam long productId, @RequestParam long userId) {
-		return customerproductService.addCustomerProduct(priority, productId, userId);
+			@RequestParam Priority priority, @PathVariable long productId) {
+		return customerproductService.addCustomerProduct(priority, productId);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "customerProduct deleted", content = {
-					@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
-			@ApiResponse(responseCode = "404", description = "customerProduct not found", content = {
-					@Content(schema = @Schema) }) })
+	@Operation(description = "**Delete CustomerProduct -**"
+			+ " the API endpoint is used to delete/remove the customer product, (authority - CUSTOMER)", responses = {
+					@ApiResponse(responseCode = "200", description = "customerProduct deleted", content = {
+							@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
+					@ApiResponse(responseCode = "404", description = "customerProduct not found", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('CUSTOMER')")
 	@DeleteMapping
 	public ResponseEntity<ResponseStructure<CustomerProductResponse>> deleteCustomerProduct(
 			@RequestParam long CustomerProductId) {
 		return customerproductService.deleteCustomerProduct(CustomerProductId);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "302", description = "customerProduct fetched", content = {
-					@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
-			@ApiResponse(responseCode = "404", description = "customerProduct not found", content = {
-					@Content(schema = @Schema) }) })
+	@Operation(description = "**Get CustomerProduct By priority -**"
+			+ " the API endpoint is used to fetch the customerProduct by the priority place (WISHLIST or CART),"
+			+ " (authority - CUSTOMER)", responses = {
+					@ApiResponse(responseCode = "302", description = "customerProduct fetched", content = {
+							@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
+					@ApiResponse(responseCode = "404", description = "customerProduct not found", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('CUSTOMER')")
 	@GetMapping
-	public ResponseEntity<ResponseStructure<List<CustomerProductResponse>>> getCustomerProductsByUserByPriority(
-			@RequestParam long userId, @RequestParam Priority priority) {
-		return customerproductService.getCustomerProductsByUserByPriority(userId, priority);
+	public ResponseEntity<ResponseStructure<List<CustomerProductResponse>>> getCustomerProductsByPriority(
+			@RequestParam Priority priority) {
+		return customerproductService.getCustomerProductsByPriority(priority);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "customerProduct updated", content = {
-					@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
-			@ApiResponse(responseCode = "400", description = "failed to update customerProduct", content = {
-					@Content(schema = @Schema) }) })
+	@Operation(description = "**Update CustomerProduct BuyStatus -**"
+			+ " the API endpoint is used to update the BuyStatus of CustomerProduct "
+			+ "(i.e., BUY_NOW, BUY_LATER and NONE (will be added to wishlist), (authority - CUSTOMER)", responses = {
+					@ApiResponse(responseCode = "200", description = "customerProduct updated", content = {
+							@Content(schema = @Schema(implementation = CustomerProductResponse.class)) }),
+					@ApiResponse(responseCode = "400", description = "failed to update customerProduct", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
 	@PutMapping
 	public ResponseEntity<ResponseStructure<CustomerProductResponse>> updateCustomerProductBuyStatus(
 			@RequestParam long customerProductId, @RequestParam BuyStatus buyStatus) {
--- src/main/java/edu/goshop_ecommerce/controller/ProductController.java
+++ src/main/java/edu/goshop_ecommerce/controller/ProductController.java
@@ -4,94 +4,104 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.goshop_ecommerce.request_dto.ProductRequest;
 import edu.goshop_ecommerce.response_dto.ProductResponse;
 import edu.goshop_ecommerce.service.ProductService;
+import edu.goshop_ecommerce.util.ErrorStructure;
 import edu.goshop_ecommerce.util.ResponseStructure;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/product")
-@Tag(name = "Product", description = "Product REST API's")
+@Tag(name = "Product", description = "REST Endpoints")
 public class ProductController {
 	@Autowired
 	private ProductService service;
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "201", description = "product added", content = {
-					@Content(schema = @Schema(implementation = ProductResponse.class)) }),
-			@ApiResponse(responseCode = "400", description = "failed to add product", content = {
-					@Content(schema = @Schema) }) })
-	@Secured({"MERCHANT"})
-	@PostMapping
+	@Operation(description = "**Create Product -**"
+			+ "the API endpoint is used to create a product data ( authority - MERCHANT only)", responses = {
+					@ApiResponse(responseCode = "201", description = "product added", content = {
+							@Content(schema = @Schema(implementation = ProductResponse.class)) }),
+					@ApiResponse(responseCode = "400", description = "failed to add product", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('MERCHANT')")
+	@PostMapping("/brands/{brandId}/categories/{categoryId}/products")
 	public ResponseEntity<ResponseStructure<ProductResponse>> addProduct(
-			@Valid @RequestBody ProductRequest productRequest, @RequestParam long userId, @RequestParam long categoryId,
-			@RequestParam long brandId) {
-		return service.addProduct(productRequest, userId, categoryId, brandId);
+			@Valid @RequestBody ProductRequest productRequest, @PathVariable long categoryId,
+			@PathVariable long brandId) {
+		return service.addProduct(productRequest, categoryId, brandId);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "302", description = "product fetched", content = {
-					@Content(schema = @Schema(implementation = ProductResponse.class)) }),
-			@ApiResponse(responseCode = "404", description = "product not found", content = {
-					@Content(schema = @Schema) }) })
-	@GetMapping
-	public ResponseEntity<ResponseStructure<ProductResponse>> getProductById(@RequestParam long productId) {
+	@Operation(description = "**Get Product By Id -**"
+			+ " the API endpoint is used to get the Product details bassed on the Product Id", responses = {
+					@ApiResponse(responseCode = "302", description = "product fetched", content = {
+							@Content(schema = @Schema(implementation = ProductResponse.class)) }),
+					@ApiResponse(responseCode = "404", description = "product not found", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@GetMapping("/products/{productId}")
+	public ResponseEntity<ResponseStructure<ProductResponse>> getProductById(@PathVariable long productId) {
 		return service.getProductById(productId);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "302", description = "product fetched", content = {
-					@Content(schema = @Schema(implementation = ProductResponse.class)) }),
-			@ApiResponse(responseCode = "404", description = "product not found", content = {
-					@Content(schema = @Schema) }) })
-	@GetMapping("/bybrand")
-	public ResponseEntity<ResponseStructure<List<ProductResponse>>> getProductsByBrand(@RequestParam long brandId) {
+	@Operation(description = "**Get Products By Brand -** "
+			+ "the API endpoint is used to fetch the Products based on the their brand", responses = {
+					@ApiResponse(responseCode = "302", description = "product fetched", content = {
+							@Content(schema = @Schema(implementation = ProductResponse.class)) }),
+					@ApiResponse(responseCode = "404", description = "product not found", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@GetMapping("/brands/{brandId}/products")
+	public ResponseEntity<ResponseStructure<List<ProductResponse>>> getProductsByBrand(@PathVariable long brandId) {
 		return service.getProductsByBrand(brandId);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "302", description = "product fetched", content = {
-					@Content(schema = @Schema(implementation = ProductResponse.class)) }),
-			@ApiResponse(responseCode = "404", description = "product not found", content = {
-					@Content(schema = @Schema) }) })
-	@GetMapping("/bycategory")
+	@Operation(description = "**Get Product By Category -**"
+			+ " the API endpoint is used to fetch the Products based on their category", responses = {
+					@ApiResponse(responseCode = "302", description = "product fetched", content = {
+							@Content(schema = @Schema(implementation = ProductResponse.class)) }),
+					@ApiResponse(responseCode = "404", description = "product not found", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@GetMapping("/categories/{categoryId}/products")
 	public ResponseEntity<ResponseStructure<List<ProductResponse>>> getProductByCategory(
-			@RequestParam long categoryId) {
+			@PathVariable long categoryId) {
 		return service.getProductByCategory(categoryId);
 	}
 
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "product deleted", content = {
-					@Content(schema = @Schema(implementation = ProductResponse.class)) }),
-			@ApiResponse(responseCode = "404", description = "product not found", content = {
-					@Content(schema = @Schema) }) })
-	@Secured({"MERCHANT"})
-	@DeleteMapping
-	public ResponseEntity<ResponseStructure<ProductResponse>> deleteProduct(@RequestParam long productId) {
+	@Operation(description = "**Delete Product -**"
+			+ " the API endpoint is used to mark the product to be deleted (authority - MERCHANT only)", responses = {
+					@ApiResponse(responseCode = "200", description = "successfully marked the product to be deleted", content = {
+							@Content(schema = @Schema(implementation = ProductResponse.class)) }),
+					@ApiResponse(responseCode = "404", description = "product not found by given Id", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('MERCHANT')")
+	@DeleteMapping("/products/{productId}")
+	public ResponseEntity<ResponseStructure<ProductResponse>> deleteProduct(@PathVariable long productId) {
 		return service.deleteProduct(productId);
 	}
 
-	@PutMapping
-	public ResponseEntity<ResponseStructure<ProductResponse>> updateProduct(@RequestParam long productId,
-			@Valid @RequestBody ProductRequest productRequest, @RequestParam long userId, @RequestParam long categoryId,
-			@RequestParam long brandId) {
-		return service.updateProduct(productId, productRequest, userId, categoryId, brandId);
+	@Operation(description = "**Update Product -**"
+			+ "the API endpoint is used to update the product (authority - MERCHANT only)", responses = {
+					@ApiResponse(responseCode = "200", description = "successfully update the product data", content = {
+							@Content(schema = @Schema(implementation = ProductResponse.class)) }),
+					@ApiResponse(responseCode = "404", description = "product not found by given Id", content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class)) }) })
+	@PreAuthorize("hasAuthority('MERCHANT')")
+	@PutMapping("/products/{productId}")
+	public ResponseEntity<ResponseStructure<ProductResponse>> updateProduct(@PathVariable long productId,
+			@Valid @RequestBody ProductRequest productRequest) {
+		return service.updateProduct(productId, productRequest);
 	}
 
 }
