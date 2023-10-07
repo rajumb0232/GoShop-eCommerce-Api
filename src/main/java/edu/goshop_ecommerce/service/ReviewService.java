f47fe2489d07753204a7d2b505cc8f3e042da5d6
rajumb0232 <rajugowda0232@gmail.com>
2023-10-07 09:13:00 +0530

    updated the contrroler and service layer of review entity for code optimization and swagger documentation

--- src/main/java/edu/goshop_ecommerce/service/ReviewService.java
+++ src/main/java/edu/goshop_ecommerce/service/ReviewService.java
@@ -11,16 +11,13 @@ import org.springframework.stereotype.Service;
 
 import edu.goshop_ecommerce.dao.ProductDao;
 import edu.goshop_ecommerce.dao.ReviewDao;
-import edu.goshop_ecommerce.dao.UserDao;
 import edu.goshop_ecommerce.entity.Product;
 import edu.goshop_ecommerce.entity.Review;
 import edu.goshop_ecommerce.entity.User;
-import edu.goshop_ecommerce.enums.UserRole;
 import edu.goshop_ecommerce.exception.ProductNotFoundById;
 import edu.goshop_ecommerce.exception.ReviewNotFoundByIdException;
-import edu.goshop_ecommerce.exception.UserIsNotACustomerException;
-import edu.goshop_ecommerce.exception.UserNotFoundByIdException;
 import edu.goshop_ecommerce.request_dto.ReviewRequest;
+import edu.goshop_ecommerce.util.ResponseEntityProxy;
 import edu.goshop_ecommerce.util.ResponseStructure;
 
 @Service
@@ -29,63 +26,60 @@ public class ReviewService {
 	@Autowired
 	private ReviewDao reviewDao;
 	@Autowired
-	private UserDao userDao;
-	@Autowired
 	private ProductDao productDao;
 	@Autowired
 	private ModelMapper modelMapper;
+	@Autowired
+	private AuthService authService;
+	@Autowired
+	private ResponseEntityProxy responseEntity;
 
-	public ResponseEntity<ResponseStructure<Review>> addReview(long userId, long productId,
-			ReviewRequest reviewRequest) {
+	public ResponseEntity<ResponseStructure<Review>> addReview(long productId, ReviewRequest reviewRequest) {
 		Review review = this.modelMapper.map(reviewRequest, Review.class);
-		User user = userDao.findUserById(userId);
-		if (user != null) {
-			if (user.getUserRole().equals(UserRole.CUSTOMER)) {
-				Product product = productDao.findProduct(productId);
-				if (product != null) {
-					float totalRating = 0;
-					List<Review> reviews = product.getReviews();
-					for (Review review2 : reviews) {
-						totalRating += review2.getRating();
-					}
-					float avgRating = totalRating / reviews.size();
-					review.setUserId(userId);
-					review.setUserName(user.getUserFirstName());
-					reviewDao.addReview(review);
-					product.setRating(avgRating);
-					productDao.addProduct(product);
-					ResponseStructure<Review> responseStructure = new ResponseStructure<>();
-					responseStructure.setStatus(HttpStatus.CREATED.value());
-					responseStructure.setMessage("Review added");
-					responseStructure.setData(review);
-					return new ResponseEntity<ResponseStructure<Review>>(responseStructure, HttpStatus.CREATED);
-				} else {
-					throw new ProductNotFoundById("Product not found");
-				}
-			} else {
+		User user = authService.getAutheticatedUser();
+		Product product = productDao.findProduct(productId);
 
-				throw new UserIsNotACustomerException("User is not customer");
+		if (product != null) {
+			float totalRating = 0;
+			List<Review> reviews = product.getReviews();
+
+			for (Review review2 : reviews) {
+				totalRating += review2.getRating();
 			}
-		} else {
-			throw new UserNotFoundByIdException("User not found");
-		}
 
+			float avgRating = totalRating / reviews.size();
+			review.setUserId(user.getUserId());
+			review.setUserName(user.getUserFirstName());
+			reviewDao.addReview(review);
+
+			product.setRating(avgRating);
+			productDao.addProduct(product);
+
+			return responseEntity.getResponseEntity(review, "Review added successfully", HttpStatus.CREATED);
+			
+		} else {
+			throw new ProductNotFoundById("Product not found");
+		}
 	}
 
 	public ResponseEntity<ResponseStructure<Review>> updateReview(long reviewId, ReviewRequest reviewRequest) {
+		User user = authService.getAutheticatedUser();
 		Review review = this.modelMapper.map(reviewRequest, Review.class);
 		Optional<Review> optionalReview = reviewDao.getReviewById(reviewId);
+		
 		if (optionalReview.isPresent()) {
 			Review exReview = optionalReview.get();
-			review.setReviewId(reviewId);
-			review.setUserId(exReview.getUserId());
-			review.setUserName(exReview.getUserName());
-			reviewDao.addReview(review);
-			ResponseStructure<Review> responseStructure = new ResponseStructure<>();
-			responseStructure.setStatus(HttpStatus.CREATED.value());
-			responseStructure.setMessage("Review updated");
-			responseStructure.setData(review);
-			return new ResponseEntity<ResponseStructure<Review>>(responseStructure, HttpStatus.CREATED);
+			
+			if(exReview.getUserId() == user.getUserId()) {
+				review.setReviewId(reviewId);
+				review.setUserId(exReview.getUserId());
+				review.setUserName(exReview.getUserName());
+				reviewDao.addReview(review);
+				
+				return responseEntity.getResponseEntity(review, "Review updated successfully", HttpStatus.OK);
+				
+			}else
+				throw new UnAuthorizedToAccessException("Failed to update the review data");
 		} else {
 			throw new ReviewNotFoundByIdException("Failed to update Review.");
 		}
